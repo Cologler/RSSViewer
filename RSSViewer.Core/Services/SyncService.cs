8edcbc4f8645388163663f863d4657a322ddbbb2
Cologler <skyoflw@gmail.com>
2020-03-15 22:37:38 +0800

    Update SyncService.cs

--- RSSViewer.Core/Services/SyncService.cs
+++ RSSViewer.Core/Services/SyncService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using RSSViewer.Abstractions;
 using RSSViewer.LocalDb;
 using RSSViewer.Utils;
 using System;
@@ -32,35 +33,62 @@ namespace RSSViewer.Services
             {
                 var sources = scope.ServiceProvider.GetRequiredService<SyncSourceManager>().GetSyncSources();
                 var ctx = scope.ServiceProvider.GetRequiredService<LocalDbContext>();
-                var provMap = ctx.SyncSourceInfos.ToDictionary(z => z.SyncSourceId);
+                var syncInfoTable = ctx.SyncSourceInfos.ToDictionary(z => z.SyncSourceId);
                 foreach (var source in sources)
                 {
-                    var provInfo = provMap.GetValueOrDefault(source.SyncSourceId);
-                    if (provInfo == null)
-                    {
-                        provInfo = new SyncSourceInfo { SyncSourceId = source.SyncSourceId };
-                        ctx.Add(provInfo);
-                    }
-
-                    var page = await source.TryGetItemsAsync(provInfo.LastSyncId, CancellationToken.None);
-                    if (page.LastId is int lastId)
-                    {
-                        provInfo.LastSyncId = lastId;
-                    }
-
-                    var newItems = page.GetItems().Select(z =>
-                    {
-                        var r = new RssItem();
-                        r.UpdateFrom(z);
-                        return r;
-                    }).ToList();
-
-                    ctx.AddOrIgnoreRange(newItems);
-                    ctx.SaveChanges();
+                    var syncInfo = syncInfoTable.GetValueOrDefault(source.SyncSourceId);
+                    await SyncCoreAsync(ctx, source, syncInfo, CancellationToken.None).ConfigureAwait(false);
                 }
+                ctx.SaveChanges();
             }
 
             this.OnSynced?.Invoke();
+        }
+
+        public Task SyncAsync(ISyncSource syncSource)
+        {
+            if (syncSource is null)
+                throw new ArgumentNullException(nameof(syncSource));
+
+            return Task.Run(async () =>
+            {
+                using (var scope = this._serviceProvider.CreateScope())
+                {
+                    var ctx = scope.ServiceProvider.GetRequiredService<LocalDbContext>();
+                    var syncInfo = ctx.SyncSourceInfos
+                        .FirstOrDefault(z => z.SyncSourceId == syncSource.SyncSourceId);
+                    await SyncCoreAsync(ctx, syncSource, syncInfo, CancellationToken.None).ConfigureAwait(false);
+                    ctx.SaveChanges();
+                }
+
+                this.OnSynced?.Invoke();
+            });
+        }
+
+        private static async Task SyncCoreAsync(LocalDbContext ctx, ISyncSource syncSource, SyncSourceInfo syncInfo, 
+            CancellationToken cancellationToken)
+        {
+            if (syncInfo is null)
+            {
+                syncInfo = new SyncSourceInfo { SyncSourceId = syncSource.SyncSourceId };
+                ctx.Add(syncInfo);
+            }
+
+            var page = await syncSource.TryGetItemsAsync(syncInfo.LastSyncId, cancellationToken)
+                .ConfigureAwait(false);
+            if (page.LastId is int lastId)
+            {
+                syncInfo.LastSyncId = lastId;
+            }
+
+            var newItems = page.GetItems().Select(z =>
+            {
+                var r = new RssItem();
+                r.UpdateFrom(z);
+                return r;
+            }).ToList();
+
+            ctx.AddOrIgnoreRange(newItems);
         }
     }
 }
