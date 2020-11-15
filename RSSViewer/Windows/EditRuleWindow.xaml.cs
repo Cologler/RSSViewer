201d7789c408a0d6e7cbe79fe7789c4aba6cd961
Cologler <skyoflw@gmail.com>
2020-11-16 05:17:21 +0800

    refactor

--- RSSViewer/Windows/EditRuleWindow.xaml.cs
+++ RSSViewer/Windows/EditRuleWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Jasily.ViewModel;
+
+using Microsoft.Extensions.DependencyInjection;
 
 using RSSViewer.Abstractions;
 using RSSViewer.Configuration;
@@ -33,6 +35,7 @@ namespace RSSViewer.Windows
         public EditRuleWindow()
         {
             this.InitializeComponent();
+            this.DataContext = new DataContextViewModel();
 
             this._handlersService = App.RSSViewerHost.ServiceProvider.GetRequiredService<RssItemHandlersService>();
             var handlers = this._handlersService.GetRuleTargetHandlers();
@@ -40,6 +43,8 @@ namespace RSSViewer.Windows
             this.ActionsList.ItemsSource = handlers;
             this.ActionsList.SelectedItem = this._handlersService.GetDefaultRuleTargetHandler();
         }
+
+        DataContextViewModel ViewModel => (DataContextViewModel)this.DataContext;
 
         public void LoadFromConf(MatchRule rule)
         {
@@ -56,16 +61,8 @@ namespace RSSViewer.Windows
                 this.ActionsList.SelectedItem = this._handlersService.GetRuleTargetHandlers()
                     .FirstOrDefault(z => z.Id == rule.HandlerId);
             }
-            this.LastMatchedText.Text = rule.LastMatched.ToLocalTime().ToLongDateString();
-            this.TotalMatchedCountText.Text = rule.TotalMatchedCount.ToString();
-            this.AutoDisabledAt.Text = rule.AutoDisabledAfterLastMatched is null
-                ? "Never"
-                : (rule.LastMatched.ToLocalTime() + rule.AutoDisabledAfterLastMatched.Value).ToLongDateString();
-            this.AutoExpiredAt.Text = rule.AutoExpiredAfterLastMatched is null
-                ? "Never"
-                : (rule.LastMatched.ToLocalTime() + rule.AutoExpiredAfterLastMatched.Value).ToLongDateString();
 
-            // 
+            // match
             this.SelectedMatchStringMode = rule.Mode;
             this.MatchValueTextBox.Text = rule.Argument;
             switch (rule.Mode)
@@ -79,6 +76,11 @@ namespace RSSViewer.Windows
                     this.SelectedRegexOptions = rule.OptionsAsRegexOptions;
                     break;
             }
+
+            this.LastMatchedText.Text = rule.LastMatched.ToLocalTime().ToLongDateString();
+            this.TotalMatchedCountText.Text = rule.TotalMatchedCount.ToString();
+
+            this.ViewModel.From(rule);
         }
 
         public void WriteToConf(MatchRule rule)
@@ -86,6 +88,10 @@ namespace RSSViewer.Windows
             if (rule is null)
                 throw new ArgumentNullException(nameof(rule));
 
+            // general
+            rule.HandlerId = ((IRssItemHandler)this.ActionsList.SelectedItem).Id;
+
+            // match
             rule.Mode = this.SelectedMatchStringMode;
             rule.Argument = this.MatchValueTextBox.Text;
             switch (rule.Mode)
@@ -100,7 +106,7 @@ namespace RSSViewer.Windows
                     break;
             }
 
-            rule.HandlerId = ((IRssItemHandler)this.ActionsList.SelectedItem).Id;
+            this.ViewModel.Write(rule);
         }
 
         private void SelectModeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -187,7 +193,19 @@ namespace RSSViewer.Windows
             {
                 MessageBox.Show("Please select a handler", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
-            }                
+            }
+
+            // check lifetime
+            if (this.ViewModel.IsEnabledAutoDisabled && !int.TryParse(this.ViewModel.AutoDisabledAfterDaysText, out _))
+            {
+                MessageBox.Show("Auto disabled days is not a integer", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (this.ViewModel.IsEnabledAutoExpired && !int.TryParse(this.ViewModel.AutoExpiredAfterDaysText, out _))
+            {
+                MessageBox.Show("Auto expired days is not a integer", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
 
             // check match
             if (this.TryCreateStringMatcher() == null)
@@ -250,6 +268,132 @@ namespace RSSViewer.Windows
                 return true;
             }
             return false;
+        }
+
+        class DataContextViewModel : BaseViewModel
+        {
+            private DateTime _lastMatched;
+            private bool _isEnabledAutoDisabled;
+            private bool _isEnabledAutoExpired;
+            private string _autoDisabledAfterDaysText;
+            private string _autoExpiredAfterDaysText;
+
+            public bool IsEnabledAutoDisabled
+            {
+                get => _isEnabledAutoDisabled;
+                set
+                {
+                    if (this.ChangeModelProperty(ref _isEnabledAutoDisabled, value))
+                    {
+                        this.RefreshProperties(10);
+                    }
+                }
+            }
+
+            public string AutoDisabledAfterDaysText
+            {
+                get => _autoDisabledAfterDaysText; 
+                set
+                {
+                    if (this.ChangeModelProperty(ref _autoDisabledAfterDaysText, value))
+                    {
+                        this.RefreshProperties(10);
+                    } 
+                }
+            }
+
+            [ModelProperty(Group = 10)]
+            public string AutoDisabledAt
+            {
+                get
+                {
+                    if (!this.IsEnabledAutoDisabled)
+                        return "Never";
+                    if (int.TryParse(this.AutoDisabledAfterDaysText, out var days))
+                        return (this._lastMatched + TimeSpan.FromDays(days)).ToLongDateString();
+                    return "Unknown";
+                }
+            }
+
+            public bool IsEnabledAutoExpired
+            {
+                get => _isEnabledAutoExpired;
+                set
+                {
+                    if (this.ChangeModelProperty(ref _isEnabledAutoExpired, value))
+                    {
+                        this.RefreshProperties(11);
+                    }
+                }
+            }
+
+            public string AutoExpiredAfterDaysText
+            {
+                get => _autoExpiredAfterDaysText;
+                set
+                {
+                    if (this.ChangeModelProperty(ref _autoExpiredAfterDaysText, value))
+                    {
+                        this.RefreshProperties(11);
+                    }
+                }
+            }
+
+            [ModelProperty(Group = 11)]
+            public string AutoExpiredAt
+            {
+                get
+                {
+                    if (!this.IsEnabledAutoExpired)
+                        return "Never";
+                    if (int.TryParse(this.AutoExpiredAfterDaysText, out var days))
+                        return (this._lastMatched + TimeSpan.FromDays(days)).ToLongDateString();
+                    return "Unknown";
+                }
+            }
+
+            public void From(MatchRule rule)
+            {
+                this._lastMatched = rule.LastMatched.ToLocalTime();
+
+                // lifetime: disabled
+                this.IsEnabledAutoDisabled = rule.AutoDisabledAfterLastMatched.HasValue;
+                if (rule.AutoDisabledAfterLastMatched.HasValue)
+                {
+                    this.AutoDisabledAfterDaysText = rule.AutoDisabledAfterLastMatched.Value.Days.ToString();
+                }
+                else
+                {
+                    this.AutoDisabledAfterDaysText = string.Empty;
+                }
+                // lifetime: expired
+                this.IsEnabledAutoExpired = rule.AutoExpiredAfterLastMatched.HasValue;
+                if (rule.AutoExpiredAfterLastMatched.HasValue)
+                {
+                    this.AutoExpiredAfterDaysText = rule.AutoExpiredAfterLastMatched.Value.Days.ToString();
+                }
+                else
+                {
+                    this.AutoExpiredAfterDaysText = string.Empty;
+                }
+
+                this.RefreshProperties();
+            }
+
+            public void Write(MatchRule rule)
+            {
+                // lifetime
+                // lifetime: disabled
+                if (this.IsEnabledAutoDisabled)
+                {
+                    rule.AutoDisabledAfterLastMatched = TimeSpan.FromDays(int.Parse(this.AutoDisabledAfterDaysText));
+                }
+                // lifetime: expired
+                if (this.IsEnabledAutoExpired)
+                {
+                    rule.AutoExpiredAfterLastMatched = TimeSpan.FromDays(int.Parse(this.AutoExpiredAfterDaysText));
+                }
+            }
         }
     }
 }
